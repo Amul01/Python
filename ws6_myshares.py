145aa0b59270a9432e439819838b05ae1bd3a5fa
Amul Luniya <amul0128india@gmail.com>
2018-12-12 14:43:39 +0530

    Add files via upload

--- ws6_myshares.py
+++ ws6_myshares.py
@@ -1,31 +1,60 @@
 import requests
 from lxml import html
-from bs4 import BeautifulSoup
 
+today=[]
+nesco=['NESCO']
+ongc=['ONGC']
+coal=['Coal India']
+prev=[]
+stocks=['NESCO','ONGC','Coal India']
+cost = [589.0, 187.3, 311.0]
+diff=[]
+
+print('\nStock\t\tToday\t\tYesterday\tCost\tCurrent difference\n')
 nescopage = requests.get('https://www.moneycontrol.com/india/stockpricequote/diversified/nesco/NES')
 nescotree = html.fromstring(nescopage.content)
-nesco = nescotree.xpath('//*[@id="Bse_Prc_tick"]/strong/text()')
-print('NESCO = '+nesco[0])
+nesco.append(float(nescotree.xpath('//*[@id="Bse_Prc_tick"]/strong/text()')[0]))
+nesco.append(float(nescotree.xpath('//*[@id="b_prevclose"]/strong/text()')[0]))
+today.append(nesco[1])
+prev.append(nesco[2])
 
 ongcpage = requests.get('https://www.moneycontrol.com/india/stockpricequote/oil-drilling-and-exploration/oilnaturalgascorporation/ONG')
 ongctree = html.fromstring(ongcpage.content)
-ongc = ongctree.xpath('//*[@id="Bse_Prc_tick"]/strong/text()')
-print('ONGC = '+ongc[0])
+ongc.append(float(ongctree.xpath('//*[@id="Bse_Prc_tick"]/strong/text()')[0]))
+ongc.append(float(ongctree.xpath('//*[@id="b_prevclose"]/strong/text()')[0]))
+today.append(ongc[1])
+prev.append(ongc[2])
 
 coalpage = requests.get('https://www.moneycontrol.com/india/stockpricequote/mining-minerals/coalindia/CI11')
 coaltree = html.fromstring(coalpage.content)
-coal = coaltree.xpath('//*[@id="Bse_Prc_tick"]/strong/text()')
-print('Coal India = '+coal[0])
+coal.append(float(coaltree.xpath('//*[@id="Bse_Prc_tick"]/strong/text()')[0]))
+coal.append(float(coaltree.xpath('//*[@id="b_prevclose"]/strong/text()')[0]))
+today.append(coal[1])
+prev.append(coal[2])
 
-#a = coaltree.xpath('//*[@id="content_bse"]/div[1]/text()')
-#print(a)
+for i in range(3): diff.append(today[i]-cost[i])
 
-#soup = BeautifulSoup(coalpage.text,'html.parser')
-#print(soup.prettify)
-#print(soup.title.get_text())
-#print(soup.find_all('p'))
-#print(soup.p)
-#print(soup.get_text())
+#print(today)
+#print(prev)
+#for i in nesco: print(i,end='\t')
 
-#l = soup.find({"class":"brdb PB5"})
-#print(l)
+for i in range(len(today)):
+    if(stocks[i]=='Coal India'): print(stocks[i],'\t',('%.2f' % today[i]),'\t',('%.2f' % prev[i]),'\t',('%.2f' % cost[i]),'\t',('%.2f' % diff[i]))
+    else: print(stocks[i],'\t\t',('%.2f' % today[i]),'\t',('%.2f' % prev[i]),'\t',('%.2f' % cost[i]),'\t',('%.2f' % diff[i]))
+
+#-----------------------------------------------------------------------------------------
+#------------------------------DESKTOP NOTIFYING
+
+import notify2
+import time
+notify2.init('Prices')
+n = notify2.Notification(message='This is the notification') 
+
+n.set_urgency(notify2.URGENCY_NORMAL) 
+n.set_timeout(10000) 
+n.update(today[1])
+n.show() 
+
+time.sleep(15) 
+
+print(input('Press Enter to stop'))
